b6b6a5ad2dc034e65b99743589317d5f7f2003a9
shahnawaz8 <shahnawaz846@gmail.com>
2024-02-16 05:29:49 +0530

    build

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var express = require('express');
 const mongoose = require('mongoose');
 const connect = require('./configs/db');
 var app = express();
+var path = require('path');
+
+var ejs = require('ejs');
 const cors = require('cors');
 app.use(express.json({ limit: '5mb' }));
 app.use(cors());
 ObjectId = mongoose.Types.ObjectId;
-
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 const userRoute = require('./routes/user');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/health',(req,res)=>{return res.send('ok');})
 app.use('/user', userRoute);
 
